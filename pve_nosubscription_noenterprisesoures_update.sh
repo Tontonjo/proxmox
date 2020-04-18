5b8243f462926a79c239aadbbac03d50ffae019d
Tontonjo <60965766+Tontonjo@users.noreply.github.com>
2020-04-18 11:02:03 +0200

    Update pve_nosubscription_noenterprisesoures_update.sh

--- pve_nosubscription_noenterprisesoures_update.sh
+++ pve_nosubscription_noenterprisesoures_update.sh
@@ -23,31 +23,32 @@ varversion=1.1
 # sed -i 's/^/#/' /etc/apt/sources.list.d/pve-enterprise.list
 # apt-get -y update
 # apt-get -y upgrade
-
+echo "----------------------------------------------------------------"
 echo "Tonton Jo - 2020"
 echo "Proxmox subscription and sources inital setup V$varversion"
+echo "----------------------------------------------------------------"
 #1: Defining distribution name:
 
-echo "defining distribution name for sources list"
+echo "- Defining distribution name for sources list"
 distribution=$(. /etc/*-release;echo $VERSION_CODENAME)
 
 #2: Remove Subscription:
 
-echo "removing No Valid Subscription Message"
+echo "- Removing No Valid Subscription Message"
 sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
 
 #3: Edit sources list:
 
-echo "Checkin Source list"
+echo "- Checkin Sources list"
 if grep -Fxq "deb http://download.proxmox.com/debian/pve $distribution pve-no-subscription" /etc/apt/sources.list
 then
     echo "Source looks alredy configured - skipping"
 else
-    echo "adding sources list"
+    echo "Adding new entry to sources.list"
     sed -i "\$adeb http://download.proxmox.com/debian/pve $distribution pve-no-subscription" /etc/apt/sources.list
 fi
 
-echo "Checking Enterprise Source list"
+echo "- Checking Enterprise Source list"
 if grep -Fxq "#deb https://enterprise.proxmox.com/debian/pve $distribution pve-enterprise" /etc/apt/sources.list.d/pve-enterprise.list
 then
     echo "Entreprise repo looks already commented - Skipping"
