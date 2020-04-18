8f3f8558226d6f830a9f267c33893e172e0ab03a
Tontonjo <60965766+Tontonjo@users.noreply.github.com>
2020-04-18 10:14:32 +0200

    Update pve_nosubscription_noenterprisesoures_update.sh

--- pve_nosubscription_noenterprisesoures_update.sh
+++ pve_nosubscription_noenterprisesoures_update.sh
@@ -12,13 +12,12 @@ varversion=1.0
 #https://www.sysorchestra.com/remove-proxmox-5-3-no-valid-subscription-message/
 #https://www.svennd.be/proxmox-ve-5-0-fix-updates-upgrades/
 
-#I assume you know what you are doing and have a default configuration.
+#I assume you know what you are doing have a backup and have a default configuration.
 
 #If you want to manually enter commands below in order: enter thoses without "#" :-)
 
 # distribution=$(. /etc/*-release;echo $VERSION_CODENAME)
 # sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
-# cp -n "/etc/apt/sources.list" "/etc/apt/sources.list.BCK"
 # echo "deb http://download.proxmox.com/debian/pve $distribution pve-no-subscription" > /etc/apt/sources.list
 # sed -i 's/^/#/' /etc/apt/sources.list.d/pve-enterprise.list
 # apt-get -y update
@@ -38,10 +37,7 @@ echo "removing No Valid Subscription Message"
 sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
 
 #3: Edit sources list:
-# -n for not overwritting if file already exist
 
-echo "backuping actual file -just in case."
-cp -n "/etc/apt/sources.list" "/etc/apt/sources.list.BCK"
 echo "Checkin Source list"
 if grep -Fxq "deb http://download.proxmox.com/debian/pve $distribution pve-no-subscription" /etc/apt/sources.list
 then
