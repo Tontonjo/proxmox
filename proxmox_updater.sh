143d36d79700a9618b55951b60976f134f543756
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-11-04 08:25:07 +0100

    Change way to remove subscription message
    As found by Adrien Linuxtricks, inverting the condition is way smarter and works way better.
    https://www.linuxtricks.fr/wiki/proxmox-quelques-infos

--- proxmox_updater.sh
+++ proxmox_updater.sh
@@ -13,18 +13,20 @@
 # You can run this script directly using:
 # wget -q -O - https://raw.githubusercontent.com/Tontonjo/proxmox/master/proxmox_updater.sh | bash
 
-version=3.1
+version=3.2
 # V1.0: Initial Release with support for both PVE and PBS
 # V2.0: Old scripts points there now :-)
 # V2.1: Some corrections and enhancements in the subscription part
 # V2.2: Enhanced sileeeeence
 # V3.0: removed repository configuration wich is now in Proxmox Toolbox - Enhanced configuration for no subscription. Tool now named "proxmox_updater.sh"
 # V3.1: correction to no-subscription message removal detection
+# V3.2: Much better and smarter way to remove subscription message (credits to @adrien Linuxtricks)
 
 # Sources:
 # https://pve.proxmox.com/wiki/Package_Repositories
 # https://www.sysorchestra.com/remove-proxmox-5-3-no-valid-subscription-message/
 # https://www.svennd.be/proxmox-ve-5-0-fix-updates-upgrades/
+# https://www.linuxtricks.fr/wiki/proxmox-quelques-infos
 
 # I assume you know what you are doing, have a backup and have a default configuration.
 
@@ -52,10 +54,10 @@ if grep -q "no-subscription" /etc/apt/sources.list; then
 	else
 		if [ -d "$pve_log_folder" ]; then
 			echo "- Removing No Valid Subscription Message for PVE"
-			sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" $proxmoxlib && systemctl restart pveproxy.service
+			sed -Ezi.bak "s/!== 'active'/== 'active'/g" $proxmoxlib && systemctl restart pveproxy.service
 		else 
 			echo "- Removing No Valid Subscription Message for PBS"
-			sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" $proxmoxlib && systemctl restart proxmox-backup-proxy.service
+			sed -Ezi.bak "s/!== 'active'/== 'active'/g" $proxmoxlib && systemctl restart proxmox-backup-proxy.service
 		fi
 	fi
 else
