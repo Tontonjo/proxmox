598be487468b0317504fad9c39cc43f4f47f64bb
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2023-08-26 19:31:38 +0200

    1.3
    Use sar from sysstat in order to have a real average value

--- cpu_scale.sh
+++ cpu_scale.sh
@@ -4,15 +4,16 @@
 # Join me on Youtube: https://www.youtube.com/c/tontonjo
 
 # Usage:
-# put scrit where you want
+# put script where you want it to be executed
 # chmod +x /path/to/cpu_scale.sh
 # Edit crontab to run script at every reboot ->
-# @reboot  bash "/root/cpu_scale.sh" >/dev/null 2>&1"
+# @reboot  bash "/path/to/cpu_scale.sh" >/dev/null 2>&1"
 # Reboot
 # Test :)
 
 # Version 1.0: Proof of concept
 # Version 1.1: Loop is better than cron, will run everytime the script end
+# Version 1.2: Use sar to get accurate average cpu load value
 
 # --------------------- Settings ------------------------------------
 averageloadupscaletime=3					# time to get average CPU load value in order to upscale
@@ -22,11 +23,14 @@ lowloadgouvernor=powersave		# CPU Scheduler to use when low usage
 upscalevalue=50					# At wich usage when LOW load gouvernor is set the CPU will upscale to hi load
 
 highloadgouvernor=schedutil		# CPU Scheduler to use when low usage
-downscalevalue=15				# At wich usage when HIGH load gouvernor is set the CPU will downscale to low load
-# --------------------- Settings ------------------------------------
+downscalevalue=10				# At wich usage when HIGH load gouvernor is set the CPU will downscale to low load
+# --------------------- Settings -------------------------------------
 # ------------------- Env Variables ----------------------------------
 execdir=$(dirname $0)
 # ------------------- Env Variables ----------------------------------
+if [ $(dpkg-query -W -f='${Status}' sysstat 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+	apt-get install -y sysstat;
+fi
 echo "- Starting Script" >> $execdir/cpu_scale.log
 while true; do 
 # --------------------- loop Variables ------------------------------------
@@ -35,21 +39,23 @@ date=$(date +%Y_%m_%d-%H_%M_%S)
 # --------------------- loop Variables ------------------------------------
 # In order to not rank up or down too fast, define the average value to use
 if echo "$actualgouvernor" | grep -Eqi "$lowloadgouvernor"; then
-	cpuload=$(echo "$[100-$(vmstat 1 $averageloadupscaletime|tail -1|awk '{print $15}')]")
+	cpuload=$(echo "$[100-$(sar -u 1 $averageloadupscaletime | awk '/^Average:/ { printf(" %.0f\n", $8)}')]")
 else	
-	cpuload=$(echo "$[100-$(vmstat 1 $averageloaddownscaletime|tail -1|awk '{print $15}')]")
+	cpuload=$(echo "$[100-$(sar -u 1 $averageloaddownscaletime | awk '/^Average:/ { printf(" %.0f\n", $8)}')]")
 fi
 
 # If the actual gouvernor is the low load gouvernor, check if CPU load is above upscale value
 if echo "$actualgouvernor" | grep -Eqi "$lowloadgouvernor"; then
 	if (( $(echo "$cpuload > $upscalevalue"))); then
 		echo "- $date - Upscaling CPU power to $highloadgouvernor at $upscalevalue% CPU load" >> $execdir/cpu_scale.log
+		echo "- $date - Upscaling CPU power to $highloadgouvernor at $upscalevalue% CPU load"
 		echo "$highloadgouvernor" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 	fi
 # If the actual gouvernor is the high load gouvernor, check if value is under downscale value
 elif echo "$actualgouvernor" | grep -Eqi "$highloadgouvernor"; then
 	if (( $(echo "$cpuload < $downscalevalue"))) ; then
 		echo "- $date - Downscaling CPU power to $lowloadgouvernor at $downscalevalue% CPU load" >> $execdir/cpu_scale.log
+		echo "- $date - Downscaling CPU power to $lowloadgouvernor at $downscalevalue% CPU load"
 		echo "$lowloadgouvernor" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 	fi
 # If none of the above, define the low load CPU Gouvernor
